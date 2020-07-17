22ba7bdf1581ca24adedb83ad24dbece68ed46a0
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-17 21:39:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   new_array << sentence.split(" ")
   next_array = []
   new_array.each do |word|
-    next_array = new_array.reverse
+    next_array << word.reverse
   end
   next_array
 end
