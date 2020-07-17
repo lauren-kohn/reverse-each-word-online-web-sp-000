51a6cdd5aa00e855ca36cfa21df31ad3923f218d
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-17 21:41:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
-  new_array = []
-  new_array << sentence.split(" ")
-  next_array = []
+  #new_array = []
+  #new_array << sentence.split(" ")
+  #next_array = []
   new_array.each do |word|
     next_array << word.reverse
   end
